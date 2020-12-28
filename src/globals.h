c49e40c09f25ee365363769cd1072ee2e148e851
Uwe Fechner <fechner@aenarete.eu>
2020-12-28 09:13:14 +0100

    Cleanup

--- src/globals.h
+++ src/globals.h
@@ -1,9 +1,9 @@
 /**
  * @file globals.h
- * @author Bernhard Kraemer
+ * @author Bernhard Kraemer, Uwe Fechner
  * @brief Project globals
- * @version 0.1
- * @date 2020-11-18
+ * @version 0.2
+ * @date 2020-12-28
  * 
  * @copyright Copyright (c) 2020
  * 
@@ -11,7 +11,6 @@
 
 #ifndef GLOBALS_H_
 #define GLOBALS_H_
-#include <app_memory/app_memdomain.h>
 
 /*** Constants **********************************************************************/
 
@@ -22,7 +21,7 @@ typedef struct {
 }__attribute__((aligned(4))) data_message_t;
 
 /*** Variables **********************************************************************/
-extern bool blink_stat; /* true = blinking, false = off */
-extern const k_tid_t led_id;
+extern bool blink_stat;      /* true = blinking, false = off */
+extern const k_tid_t led_id; /* the id of the led thread */
 
 #endif /* GLOBALS_H_ */
